1bf2319d8c05d7b71879589e4612809003b88feb
nadocesar <nadocesarsantos@gmail.com>
2022-10-06 01:28:02 -0300

    saida da função

--- script.js
+++ script.js
@@ -24,30 +24,41 @@ main.appendChild(inputBox)
 
 var resultado = document.createElement('input')//Saida com o Calculo
 resultado.type = 'text'
+resultado.id = 'resultado'
 container.appendChild(resultado)
+
+var resposta = document.createElement('h3')
+
+container.appendChild(resposta)
 
 const btn = document.createElement('button')//Botao função do calculo
 btn.classname = 'btn'
 btn.type = 'button'
 btn.innerText = 'Calcular'
 btn.addEventListener('click', () => {
-    
+   //const resposta = document.getElementById('resultado')
    var RA = parseInt(document.getElementById('inputBox').value)
     //var RA = Event.inputBox
     //let RA = parseInt(document.getElementById('.inputBox'))
     //console.log(RA)
     for (cont = 1; cont <= RA; cont ++){ 
         if(RA % cont == 0){
-           resultado = cont //(cont +' ')
-           console.log(cont)
-           resultado = document.getElementById('cont')
+           resposta = cont //(cont +' ')
+           console.log(resposta)
+           //resultado = document.getElementById(cont)
+           document.write(resposta)
+           
         }
     }
+    
+    //var resultado = cont;
+    //var resultado = Number(document.getElementById('cont'))
+    //resultado = cont
     
 })
 main.appendChild(btn)
 
-resultado = document.getElementById('cont')
+//resultado = document.getElementById(cont)
 
 document.body.appendChild(container)
 
@@ -67,11 +78,13 @@ document.body.appendChild(container)
 // container.appendChild(resultado)
 
 // var n = parseInt(prompt("Digite um numero"))
-// function resposta(){
+// function resposta = document.getElementById('p')
+//(){
 //    for(i = 1; i <= n; i++){
 //        if(n % i == 0){
 //         alert(i+" ");
 //       }
 //    }
 // }
-// resultado = resposta();
+// resultado = resposta = document.getElementById('p')
+//);
